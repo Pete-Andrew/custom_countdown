2b19bfd2e0619fcf692ea5d4c65d13bb7c443fb2
Peter Andrew <peter.a.andrew@gmail.com>
2024-11-06 14:50:06 +0000

    more js added

---
+++ script.js
@@ -0,0 +1,47 @@
+//const { default: events } = require("inquirer/lib/utils/events");
+
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span') //selects anything within a span element, e.g. time, day, week etc. returns them as an array element
+
+let countdownTitle = '';
+let countdownDate = '';
+let countdownValue = Date; //explicitly a date object
+
+const second = 1000;
+const minutes = second*60;
+const hour = minutes*60;
+const day = hour*24;
+
+// Set Date Input Min with Todays date
+const today = new Date().toISOString().split('T')[0]; //returns the 1st item of the split array
+dateEl.setAttribute('min', today); // sets the min date attribute so you can't set a past date
+
+//populate the countdown / UI
+function updateDOM () {
+    const now = new Date().getTime(); // getting current date and how far is is from 1970, returns a millisecond value
+    const distance = countdownValue - now; //countdown value is in the future
+    console.log('distance', distance); 
+
+}
+
+
+//Takes values from form input
+function updateCountdown (e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+    //Get number version of current Date, update DOM
+    countdownValue = new Date(countdownDate).getTime(); //object = new Date (Date is a type of object)
+    console.log('countdown value:', countdownValue);
+    updateDOM();
+}
+
+// Event listener
+countdownForm.addEventListener('submit', updateCountdown)
